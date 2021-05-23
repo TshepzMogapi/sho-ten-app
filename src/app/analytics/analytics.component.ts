0d86ebc768e45f21841fe8f667892275902fc87b
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-05-23 17:42:40 +0200

    set up chart js to use for analytics data

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { LinkService } from "../services/link/link.service";
+import Chart from "chart.js/auto";
 
 @Component({
   selector: "app-analytics",
@@ -9,6 +10,9 @@ import { LinkService } from "../services/link/link.service";
 })
 export class AnalyticsComponent implements OnInit {
   dataAnalytics;
+
+  @ViewChild("barCanvas") private barCanvas: ElementRef;
+  barChart: any;
   constructor(private router: Router, private linkService: LinkService) {}
 
   ngOnInit(): void {
@@ -17,7 +21,49 @@ export class AnalyticsComponent implements OnInit {
       if (res.success) {
         console.log(res);
         this.dataAnalytics = res.body;
+        this.showChart();
       }
+    });
+  }
+
+  showChart() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: "bar",
+      data: {
+        labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        datasets: [
+          {
+            label: "My First Dataset",
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(255, 205, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(201, 203, 207, 0.2)",
+            ],
+            borderColor: [
+              "rgb(255, 99, 132)",
+              "rgb(255, 159, 64)",
+              "rgb(255, 205, 86)",
+              "rgb(75, 192, 192)",
+              "rgb(54, 162, 235)",
+              "rgb(153, 102, 255)",
+              "rgb(201, 203, 207)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
     });
   }
 }
